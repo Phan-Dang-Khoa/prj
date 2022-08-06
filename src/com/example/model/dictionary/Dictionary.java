d69a57f74672c15d93df268f357e8680387ce463
Phan-Dang-Khoa <66957316+Phan-Dang-Khoa@users.noreply.github.com>
2022-08-06 12:17:21 +0700

    Add files via upload

---
+++ src/com/example/model/dictionary/Dictionary.java
@@ -0,0 +1,25 @@
+package com.example.model.dictionary;
+
+import com.example.model.word.Word;
+
+import java.util.ArrayList;
+
+public class Dictionary {
+    private ArrayList<Word> words;
+
+    public ArrayList<Word> getWords() {
+        return words;
+    }
+
+    public void setWords(ArrayList<Word> words) {
+        this.words = words;
+    }
+
+    public Dictionary(ArrayList<Word> words) {
+        this.words = words;
+    }
+
+    public Dictionary() {
+    }
+
+}
